dd09fa0d9d4c56610c909c2aaca866ce3c425e11
zainhabib <zain.n.habib@gmail.com>
2022-08-24 17:49:40 -0500

    Clean up

--- src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
@@ -1,8 +1,5 @@
 package guru.springframework.sfgdi.services;
 
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
-
 /**
  * Created by jt on 12/27/19.
  */
--- src/main/java/guru/springframework/sfgdi/services/I18nSpanishGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nSpanishGreetingService.java
@@ -1,8 +1,5 @@
 package guru.springframework.sfgdi.services;
 
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
-
 public class I18nSpanishGreetingService implements GreetingService{
     @Override
     public String sayGreeting() {
